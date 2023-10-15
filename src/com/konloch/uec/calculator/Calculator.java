e0c202449fea852d1a1f068e88a50482ff410616
Konloch <konloch@gmail.com>
2023-10-15 02:39:52 -0600

    Add calculator base

---
+++ src/com/konloch/uec/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.konloch.uec.calculator;
+
+/**
+ * @author Konloch
+ * @since 10/15/2023
+ */
+public abstract class Calculator
+{
+	public abstract long add(long a, long b);
+	public abstract long sub(long a, long b);
+	public abstract long mul(long a, long b);
+	public abstract long div(long a, long b);
+}
