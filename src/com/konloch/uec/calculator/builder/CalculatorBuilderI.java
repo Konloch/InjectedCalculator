fcf48e6d431fee5e7cee4c87bc7f13b7d0f8505a
Konloch <konloch@gmail.com>
2023-10-15 02:40:06 -0600

    Add calculator builder interface

---
+++ src/com/konloch/uec/calculator/builder/CalculatorBuilderI.java
@@ -0,0 +1,12 @@
+package com.konloch.uec.calculator.builder;
+
+import com.konloch.uec.calculator.Calculator;
+
+/**
+ * @author Konloch
+ * @since 10/15/2023
+ */
+public interface CalculatorBuilderI
+{
+	Calculator build();
+}
